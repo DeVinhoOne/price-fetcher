6ee5b9c2b43747b0932b585ac8d9453df3f24ba8
dawid <caushered@gmail.com>
2023-02-01 21:00:21 +0100

    feat: get email alerts by email

--- src/main/java/com/devinho/pricefetcher/controller/AlertContoller.java
+++ src/main/java/com/devinho/pricefetcher/controller/AlertContoller.java
@@ -2,22 +2,26 @@ package com.devinho.pricefetcher.controller;
 
 import com.devinho.pricefetcher.model.dto.alert.CreateEmailAlertDto;
 import com.devinho.pricefetcher.model.dto.alert.CreateEmailAlertResponseDto;
-import com.devinho.pricefetcher.service.AlertService;
+import com.devinho.pricefetcher.service.EmailAlertService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.devinho.pricefetcher.service.EmailAlertService.*;
 
 @RestController
 @RequestMapping("/alerts")
 @RequiredArgsConstructor
 public class AlertContoller {
 
-    private final AlertService alertService;
+    private final EmailAlertService emailAlertService;
 
     @PostMapping("/create") //TODO 1. Add validation
     public CreateEmailAlertResponseDto createNewEmailAlert(@RequestBody CreateEmailAlertDto dto) {
-        return alertService.createNewEmailAlert(dto);
+        return emailAlertService.createNewEmailAlert(dto);
+    }
+
+    @GetMapping("/")
+    public Query getAlertsByEmail(@RequestHeader String email) {
+        return emailAlertService.getAlertsByEmail(email);
     }
 }
