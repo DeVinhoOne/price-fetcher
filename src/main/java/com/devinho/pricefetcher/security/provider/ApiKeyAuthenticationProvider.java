208db50a4a903e3d8a817be335043debbe83f49f
dawid <caushered@gmail.com>
2023-02-05 13:52:37 +0100

    refactor: remove imports

--- src/main/java/com/devinho/pricefetcher/security/provider/ApiKeyAuthenticationProvider.java
+++ src/main/java/com/devinho/pricefetcher/security/provider/ApiKeyAuthenticationProvider.java
@@ -2,14 +2,11 @@ package com.devinho.pricefetcher.security.provider;
 
 import com.devinho.pricefetcher.security.authentication.ApiKeyAuthentication;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class ApiKeyAuthenticationProvider implements AuthenticationProvider {
