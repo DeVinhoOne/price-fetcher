d74bdbe3a72fd1d295f824feb1845fd3ccfcf130
Dawid Łacheta <45907140+DeVinhoOne@users.noreply.github.com>
2023-02-08 13:58:27 +0100

    fix: api key (#10)

--- src/main/java/com/devinho/pricefetcher/security/config/CustomWebSecurityConfig.java
+++ src/main/java/com/devinho/pricefetcher/security/config/CustomWebSecurityConfig.java
@@ -2,6 +2,8 @@ package com.devinho.pricefetcher.security.config;
 
 import com.devinho.pricefetcher.security.filter.ApiKeyFilter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,12 +12,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @RequiredArgsConstructor
+@Slf4j
 public class CustomWebSecurityConfig {
 
     private final ApiKeyFilter apiKeyFilter;
 
+    @Value("${security-config.api-key-value}")
+    private String apiKey;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("Security setup with API-KEY: {}...", apiKey.substring(0, 7));
         return http
                 .addFilterAt(apiKeyFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests().anyRequest().authenticated()
